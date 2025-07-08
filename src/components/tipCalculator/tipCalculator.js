dec2a71910fa1329d968e292099ae6f64bc90f1e
Saket Pareek <45589783+saket-pareek@users.noreply.github.com>
2025-07-08 09:46:40 +0530

    feature/tip-calulator

---
+++ src/components/tipCalculator/tipCalculator.js
@@ -0,0 +1,18 @@
+export default function tipCalculator(el) {
+  const billAmountEl = el.querySelector('.tip-calculator__bill-amount');
+  const tipPercentageEl = el.querySelector('.tip-calculator__tip-percentage');
+  const btnEl = el.querySelector('.tip-calculator__btn');
+  const totalEl = el.querySelector('.tip-calculator__total');
+
+  const calculatePercentValue = (total, percent) => (percent / 100) * total;
+
+  const handleCalculate = e => {
+    e.preventDefault();
+    const billAmount = +billAmountEl.value;
+    const tipPercentage = +tipPercentageEl.value;
+    const tip = calculatePercentValue(billAmount, tipPercentage);
+    totalEl.textContent = (billAmount + tip).toFixed(2);
+  };
+
+  btnEl.addEventListener('click', handleCalculate);
+}
