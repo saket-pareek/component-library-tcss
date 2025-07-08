9bb7e3ed3f865773043c0bbdef8a80d483f9aa25
Saket Pareek <45589783+saket-pareek@users.noreply.github.com>
2025-07-08 09:51:04 +0530

    add util

--- src/components/tipCalculator/tipCalculator.js
+++ src/components/tipCalculator/tipCalculator.js
@@ -1,10 +1,10 @@
+import { calculatePercentValue } from '../../js/utils';
+
 export default function tipCalculator(el) {
   const billAmountEl = el.querySelector('.tip-calculator__bill-amount');
   const tipPercentageEl = el.querySelector('.tip-calculator__tip-percentage');
   const btnEl = el.querySelector('.tip-calculator__btn');
   const totalEl = el.querySelector('.tip-calculator__total');
-
-  const calculatePercentValue = (total, percent) => (percent / 100) * total;
 
   const handleCalculate = e => {
     e.preventDefault();
